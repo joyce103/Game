1203ade243d780b9f363bc74f70f45e8ecf10b99
joyce103 <joyce123415@gmail.com>
2021-03-21 23:22:30 +0800

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,356 @@
+﻿namespace final
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.start = new System.Windows.Forms.Button();
+            this.end = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::final.Properties.Resources.b;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(30, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 73);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::final.Properties.Resources.b;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(171, 30);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 73);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = global::final.Properties.Resources.b;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button3.Location = new System.Drawing.Point(316, 30);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(119, 73);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = global::final.Properties.Resources.b;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button4.Location = new System.Drawing.Point(466, 30);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(119, 73);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackgroundImage = global::final.Properties.Resources.b;
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button5.Location = new System.Drawing.Point(30, 130);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(119, 73);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackgroundImage = global::final.Properties.Resources.b;
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button6.Location = new System.Drawing.Point(171, 130);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(119, 73);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackgroundImage = global::final.Properties.Resources.b;
+            this.button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button7.Location = new System.Drawing.Point(316, 130);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(119, 73);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackgroundImage = global::final.Properties.Resources.b;
+            this.button8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button8.Location = new System.Drawing.Point(466, 130);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(119, 73);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackgroundImage = global::final.Properties.Resources.b;
+            this.button9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button9.Location = new System.Drawing.Point(30, 236);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(119, 73);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackgroundImage = global::final.Properties.Resources.b;
+            this.button10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button10.Location = new System.Drawing.Point(171, 236);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(119, 73);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "10";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button11
+            // 
+            this.button11.BackgroundImage = global::final.Properties.Resources.b;
+            this.button11.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button11.Location = new System.Drawing.Point(316, 236);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(119, 73);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "11";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button12
+            // 
+            this.button12.BackgroundImage = global::final.Properties.Resources.b;
+            this.button12.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button12.Location = new System.Drawing.Point(466, 236);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(119, 73);
+            this.button12.TabIndex = 11;
+            this.button12.Text = "12";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button13
+            // 
+            this.button13.BackgroundImage = global::final.Properties.Resources.b;
+            this.button13.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button13.Location = new System.Drawing.Point(30, 343);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(119, 73);
+            this.button13.TabIndex = 12;
+            this.button13.Text = "13";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button14
+            // 
+            this.button14.BackgroundImage = global::final.Properties.Resources.b;
+            this.button14.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button14.Location = new System.Drawing.Point(171, 343);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(119, 73);
+            this.button14.TabIndex = 13;
+            this.button14.Text = "14";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button15
+            // 
+            this.button15.BackgroundImage = global::final.Properties.Resources.b;
+            this.button15.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button15.Location = new System.Drawing.Point(316, 343);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(119, 73);
+            this.button15.TabIndex = 14;
+            this.button15.Text = "15";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button16
+            // 
+            this.button16.BackgroundImage = global::final.Properties.Resources.b;
+            this.button16.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button16.Location = new System.Drawing.Point(466, 343);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(119, 73);
+            this.button16.TabIndex = 15;
+            this.button16.Text = "16";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // start
+            // 
+            this.start.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.start.Font = new System.Drawing.Font("新細明體", 12F);
+            this.start.Location = new System.Drawing.Point(635, 236);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(136, 72);
+            this.start.TabIndex = 16;
+            this.start.Text = "開始";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            this.start.MouseDown += new System.Windows.Forms.MouseEventHandler(this.start_MouseDown);
+            this.start.MouseUp += new System.Windows.Forms.MouseEventHandler(this.start_MouseUp);
+            // 
+            // end
+            // 
+            this.end.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.end.Font = new System.Drawing.Font("新細明體", 12F);
+            this.end.Location = new System.Drawing.Point(635, 344);
+            this.end.Name = "end";
+            this.end.Size = new System.Drawing.Size(136, 72);
+            this.end.TabIndex = 17;
+            this.end.Text = "結束";
+            this.end.UseVisualStyleBackColor = true;
+            this.end.Click += new System.EventHandler(this.end_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(639, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 30);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "秒數:秒";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1500;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.end);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "翻牌遊戲";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button end;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace final
+{
+    public partial class Form1 : Form
+    {
+        int count = 0;
+        int second, second1;
+        int Num;
+        int Num2;
+        int sum = 0;
+        Color[] co = { Color.Transparent,Color.Blue, Color.BlueViolet,
+            Color.Orange, Color.Red, Color.DeepSkyBlue, Color.DeepPink,
+            Color.LawnGreen, Color.Yellow, Color.Blue, Color.BlueViolet,
+            Color.Orange, Color.Red, Color.DeepSkyBlue, Color.DeepPink,
+            Color.LawnGreen, Color.Yellow };
+        Button[] bt = new Button[17]; 
+        int[] state = new int[17]; 
+        int[] TF = { -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bt[1] = button1;
+            bt[2] = button2;
+            bt[3] = button3;
+            bt[4] = button4;
+            bt[5] = button5;
+            bt[6] = button6;
+            bt[7] = button7;
+            bt[8] = button8;
+            bt[9] = button9;
+            bt[10] = button10;
+            bt[11] = button11;
+            bt[12] = button12;
+            bt[13] = button13;
+            bt[14] = button14;
+            bt[15] = button15;
+            bt[16] = button16;
+            end.Enabled = false;
+            timer1.Enabled = false;
+            for(int i = 1; i <= 16; i++)
+            {
+                bt[i].Enabled = false;
+            }
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            count = 0;
+            sum = 0;
+            second = 0;
+            start.Enabled = false;
+            end.Enabled = true;
+            timer1.Enabled = true;
+            for(int i = 1; i <= 16; i++)
+            {
+                TF[i] = 0;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            second++;
+            label1.Text = "秒數:" +second.ToString() + "秒";
+        }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            second1++;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { 
+            if (count == 0)
+            {
+                Button btnHit = (Button)sender;
+                Num = int.Parse(btnHit.Text);
+                TF[Num] = 1;
+                bt[Num].BackgroundImage = null;
+                count++;
+            }
+            else if (count == 1)
+            {
+                Button btnHit = (Button)sender;
+                Num2 = int.Parse(btnHit.Text);
+                bt[Num2].BackgroundImage = null;
+                TF[Num2] = 1;
+                count++;
+                if (Num == Num2) { count = 1; TF[Num2] = 0; }
+            }
+            else if (count == 2)
+            {
+                count = 0;
+                if (bt[Num].BackColor != bt[Num2].BackColor)
+                {
+                    bt[Num].BackgroundImage = final.Properties.Resources.b;
+                    bt[Num2].BackgroundImage = final.Properties.Resources.b;
+                    TF[Num] = 0;
+                    TF[Num2] = 0;
+                }
+                else
+                {
+                    bt[Num].Enabled = false;
+                    bt[Num2].Enabled = false;
+                }
+            }
+            for (int i = 1; i <= 16; i++)
+            {
+                sum += TF[i];
+            }
+            if (sum==16)
+            {
+                timer1.Enabled = false;
+                MessageBox.Show("你贏了,共花了"+second.ToString()+"秒", "翻牌遊戲");
+                for (int i = 1; i <= 16; i++)
+                {
+                    bt[i].Enabled = false;
+                }
+            }
+            else
+            {
+                sum = 0;
+            }
+            
+        }
+
+        private void start_MouseDown(object sender, MouseEventArgs e)
+        {
+            
+            Random rn = new Random();
+            for (int i = 1; i <= 16; i++)
+            {
+                bt[i].Enabled = true;
+                state[i] = rn.Next(1, 17);
+                for (int j = 1; j < i; j++)
+                {
+                    while (state[i] == state[j])
+                    {
+                        j = 0;
+                        state[i] = rn.Next(1, 17);
+                    }
+                }
+                bt[i].BackColor = co[state[i]];
+                bt[i].BackgroundImage = null;
+            }
+        }
+
+        private void start_MouseUp(object sender, MouseEventArgs e)
+        {
+            
+            for (int i = 1; i <= 16; i++)
+            {
+                bt[i].BackgroundImage = final.Properties.Resources.b;
+            }
+        }
+
+        private void end_Click(object sender, EventArgs e)
+        {
+            start.Enabled = true;
+            end.Enabled = false;
+            timer1.Enabled = false;
+            for (int i=1; i <= 16; i++)
+            {
+                bt[i].Enabled = false;
+                bt[i].BackgroundImage = final.Properties.Resources.b;
+            }
+            second = 0;
+            label1.Text = "秒數:" + second.ToString() + "秒";
+        }
+
+
+    }
+}
